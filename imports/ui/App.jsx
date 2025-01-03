6f9715952d8529eaf51f5db74ec38493341c9ecf
Zeedan345 <36732475+Zeedan345@users.noreply.github.com>
2025-01-03 14:14:05 -0600

    making protected pages

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Dashboard } from "./components/dashboard/Dashboard";
 import { NotFound } from "./components/page_not_found/PageNotFound";
+import { AuthProvider } from "./contexts/AuthContext";
+import { PrivateRoute } from "./components/sign_in/PrivateRoute";
 
 export const App = () => (
-    <BrowserRouter>
-        <div>
-            <nav>
-                <Link to="/"></Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </div>
-    </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+            <div>
+                <nav>
+                    <Link to="/"></Link>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="*" element={<NotFound />} />
+                    <Route 
+                        path="/protected" 
+                        element={
+                            <PrivateRoute>
+                            </PrivateRoute>
+                        } 
+                        />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    </AuthProvider>
 );
