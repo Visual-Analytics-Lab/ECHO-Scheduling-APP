a1b25aef0f6866e8e81db72fe82bd35d2cfff624
CanonSb <canon.sbravati@yahoo.com>
2025-02-13 17:06:42 -0600

    Add build script

---
+++ prep_dev.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+meteor build --architecture os.linux.x86_64 ../build
+rm -r ../build/ESS.tar.gz
+mv ../build/ECHO-Scheduling-APP.tar.gz ../build/ESS.tar.gz
+# Change username to yours
+scp -r ../build/ESS.tar.gz cas1349@essdev.msussrc.com:/home/admin
